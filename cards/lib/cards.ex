4e322729c10c206643b355b612a034e0d5b44308
Raghavendran Suryanarayanan <raghav2787@gmail.com>
2018-12-02 20:35:21 +0000

    RS - Added documentation

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,8 +1,14 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
   def hello do
     :world
   end
 
+  @doc """
+    Returns a list of string representing a list of cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -13,11 +19,47 @@ defmodule Cards do
       end
   end
 
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+
+  @doc """
+    Divides a deck into a hand and the remainder of the deck. 
+    The `head_size` argument indicates how many cards should
+    be in the hand
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand) do
     Enum.split(deck, hand)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def readFileFromDisk(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _} -> "That file does not exist"
+    end
+    
+  end
+
+  def allInOne(hand_size) do
+    create_deck() |> shuffle() |> deal(hand_size)
+  end
+
 end
